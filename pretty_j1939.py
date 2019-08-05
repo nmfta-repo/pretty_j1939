8d2c146e8745bb4141741510aa9be66bdf4cfa6d
Ben Gardiner <ben.l.gardiner@gmail.com>
2019-08-05 15:20:54 -0400

    parse: guard pretty-j1939.py execution by testing for main invocation (Jeremy Daily)

--- pretty_j1939.py
+++ pretty_j1939.py
@@ -36,46 +36,46 @@ describer = pretty_j1939.parse.get_describer(describe_pgns=args.pgn, describe_sp
                                              describe_link_layer=args.link, describe_transport_layer=args.transport,
                                              include_transport_rawdata=args.candata,
                                              include_na=args.include_na)
+if __name__ == '__main__':
+    with open(args.candump, 'r') as f:
+        for candump_line in f.readlines():
+            if candump_line == '\n':
+                continue
+            try:
+                timestamp = float(candump_line.split(' ')[0].replace('(', '').replace(')', ''))
+                message_id = bitstring.BitString(hex=candump_line.split(' ')[2].split('#')[0])
+                message_data = bitstring.BitString(hex=candump_line.split(' ')[2].split('#')[1])
 
-with open(args.candump, 'r') as f:
-    for candump_line in f.readlines():
-        if candump_line == '\n':
-            continue
-        try:
-            timestamp = float(candump_line.split(' ')[0].replace('(', '').replace(')', ''))
-            message_id = bitstring.BitString(hex=candump_line.split(' ')[2].split('#')[0])
-            message_data = bitstring.BitString(hex=candump_line.split(' ')[2].split('#')[1])
+            except IndexError:
+                print("Warning: error in line '%s'" % candump_line, file=sys.stderr)
+                continue
+            except ValueError:
+                print("Warning: error in line '%s'" % candump_line, file=sys.stderr)
+                continue
 
-        except IndexError:
-            print("Warning: error in line '%s'" % candump_line, file=sys.stderr)
-            continue
-        except ValueError:
-            print("Warning: error in line '%s'" % candump_line, file=sys.stderr)
-            continue
+            desc_line = ''
 
-        desc_line = ''
-
-        description = describer(message_data.bytes, message_id.uint)
-        if args.format:
-            json_description = str(json.dumps(description, indent=4))
-        else:
-            json_description = str(json.dumps(description, separators=(',', ':')))
-        desc_line = desc_line + json_description
-        if args.candata:
-            can_line = candump_line.rstrip() + " ; "
-            if not args.format:
-                desc_line = can_line + desc_line
+            description = describer(message_data.bytes, message_id.uint)
+            if args.format:
+                json_description = str(json.dumps(description, indent=4))
             else:
-                formatted_lines = desc_line.splitlines()
-                if len(formatted_lines) == 0:
-                    desc_line = can_line
+                json_description = str(json.dumps(description, separators=(',', ':')))
+            desc_line = desc_line + json_description
+            if args.candata:
+                can_line = candump_line.rstrip() + " ; "
+                if not args.format:
+                    desc_line = can_line + desc_line
                 else:
-                    first_line = formatted_lines[0]
-                    desc_line = can_line + first_line
-                    formatted_lines.remove(first_line)
+                    formatted_lines = desc_line.splitlines()
+                    if len(formatted_lines) == 0:
+                        desc_line = can_line
+                    else:
+                        first_line = formatted_lines[0]
+                        desc_line = can_line + first_line
+                        formatted_lines.remove(first_line)
 
-                for line in formatted_lines:
-                    desc_line = desc_line + '\n' + ' '*len(candump_line) + "; " + line
+                    for line in formatted_lines:
+                        desc_line = desc_line + '\n' + ' '*len(candump_line) + "; " + line
 
-        if len(desc_line) > 0:
-            print(desc_line)
+            if len(desc_line) > 0:
+                print(desc_line)
